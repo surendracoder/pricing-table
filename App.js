71e06acc1bf1d3f9aaa9b220203d7e33470d3b42
surendra subedar.j <73166719+surendracoder@users.noreply.github.com>
2022-08-23 13:13:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,113 @@
+ import './App.css';
+ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+
+
+function App() { 
+  return (
+    
+    <div class="container ">
+      <div class="row">
+        <div class="col-sm-4  mt-5">
+          <h6 class="text-primary  mt-5">Pricing</h6>
+          <h1>Get lifetime access</h1>
+          <p class="mt-2">Choose from over 210+ beautiful and responsive components.</p>
+          </div>
+        <div class="col-sm-4 mt-5">
+        <div class="container py-5">
+          <div class="row tex-center ">
+            <div class="col=lg-4 mb-5  mb-lg-0">
+            
+              <div class="bg-white p-5 rounded-lg shadow">
+                <h2 class="h1 font-weight-bold  text-center">$149</h2>
+                <div class="pop">popular</div>
+                <h1 class="h6 text-uppercase font-weight-bold text-center mb-2">Application</h1>
+                <p class=" text-center ">For accessible applications</p>
+                
+        
+                 {/* <div class="custom-seperator my-4 mx-4 mx-auto bg-primary"></div> */}
+
+               <ul class="list-unstyled my-4 text-small text-lift">
+                 <li class="mb-3">
+                   <i class=" fa fa-check mr-2 text-primary"></i> Application components
+                 </li>
+
+                 <li class="mb-3">
+                 <i class="fa fa-check mr-2 text-primary "></i> Unlimited projects
+              
+                 </li>
+
+                 <li class="mb-3">
+                   <i class="fa fa-check mr-2 text-primary"></i> Lifetime access
+                 </li>
+
+                 <li class="mb-3">
+                   <i class="fa fa-check mr-2 text-primary"></i> Customer support
+                 </li>
+
+
+                 <li class="mb-3">
+                   <i class="fa fa-check  mr-2 text-primary"></i> Free updates
+                 </li>
+                 </ul>
+                 <div class="d-grid">
+               <button type="button" class="btn btn-primary btn-block p-2">Get Started</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div> 
+        <div class="col-sm-4 mt-5">
+         <div class="container py-5">
+           <div class="row tex-center">
+             <div class="col=lg-4 mb-5 mb-lg-0">
+                <div class="bg-white p-5 rounded-lg shadow"> 
+                  <h2 class="h1 font-weight-bold text-center">$149</h2>
+                  <h1 class="h6 text-uppercase font-weight-bold text-center mb-2">Marketing</h1>
+                  <p class="text-center">For impressive landing pages</p>
+                 <div class="custom-seperator my-4 mx-4 mx-auto bg-primary"></div>
+
+                 <ul class="list-unstyled my-4 text-small text-lift">
+                   <li class="mb-3">
+                     <i class=" fa fa-check mr-2 text-primary"></i>Marketing components
+                   </li>
+
+                   <li class="mb-3">
+                   <i class="fa fa-check mr-2 text-primary "></i>Unlimited projects
+                   </li>
+
+                   <li class="mb-3">
+                     <i class="fa fa-check mr-2 text-primary"></i> Lifetime access
+                   </li>
+
+                   <li class="mb-3">
+                     <i class="fa fa-check mr-2 text-primary"></i>Customer support 
+                   </li>
+
+                   <li class="mb-3">
+                     <i class="fa fa-check  mr-2 text-primary"></i>  Free updates
+                   </li>
+                
+
+                 </ul>
+
+                 <div class="d-grid">
+               <button type="button" class="btn btn-primary btn-block p-2">Get Started</button>
+              </div>
+              
+               </div>
+             </div>
+            </div>
+           </div>
+         </div>
+         </div>
+         </div>
+        
+      );
+    };
+export default App;
+
+      
+    
+  
